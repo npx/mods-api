90176d51242ccafbf6305cec5a23d510ac97d1be
Fabian Clemenz <fclemenz@outlook.com>
2016-10-30 22:25:14 +0100

    edited makeHandler to work with JSONRequest

--- handler.go
+++ handler.go
@@ -17,8 +17,12 @@ func handle404(w http.ResponseWriter, r *http.Request) {
 
 func makeHandler(endpoint Endpoint) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// creating an instance of JSONRequest
+		var rq JSONRequest
+		rq.RawRequest = r
+
 		// Run module's handler function
-		response := endpoint.Handler(r)
+		response := endpoint.Handler(&rq)
 		// Respond
 		respond(response, w)
 	}
