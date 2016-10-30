9013352e734ad356486ad1a8980978dd32a0a6a7
Fabian Clemenz <fclemenz@outlook.com>
2016-10-30 20:48:24 +0100

    added CoinModule - returns heads/tails as result

---
+++ mod_coin.go
@@ -0,0 +1,44 @@
+package modsapi
+
+import (
+	"math/rand"
+	"net/http"
+)
+
+// CoinModule is the implementation of a Coin
+type CoinModule struct{}
+
+// ID of the CoinModule, which will be used in the URL
+func (module CoinModule) ID() string {
+	return "coin"
+}
+
+// Endpoints lists all endpoints of the module
+func (module CoinModule) Endpoints() []Endpoint {
+	return []Endpoint{
+		Endpoint{"/flip", module.flipCoin},
+	}
+}
+
+// Response of the Coin flip
+type coinResponse struct {
+	Result string `json:"result"`
+}
+
+// The coinFlip function
+func (module CoinModule) flipCoin(r *http.Request) JSONResponse {
+	value := rand.Intn(2) + 1
+
+	var side string
+
+	if value == 1 {
+		side = "heads"
+	} else {
+		side = "tails"
+	}
+
+	return JSONResponse{
+		http.StatusOK,
+		coinResponse{side},
+	}
+}
