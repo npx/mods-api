ca9282c6e38045fdcc4660ed8681014d43b64dcc
FreakyTea <fclemenz@outlook.com>
2016-10-30 20:26:14 +0100

    DiceModule (#1)
    * added DiceModul - returns 1-6 as result
    
    * registered Module in main.go
    
    * renamed endpoints and rollDie function
    
    * renamed file to mod_dice.go

---
+++ mod_dice.go
@@ -0,0 +1,36 @@
+package modsapi
+
+import (
+	"math/rand"
+	"net/http"
+)
+
+// DiceModule is the implementation of a Dice
+type DiceModule struct{}
+
+// ID of the Dice Modul, which will be used in the URL
+func (module DiceModule) ID() string {
+	return "dice"
+}
+
+// Endpoints lists all endpoints of the module
+func (module DiceModule) Endpoints() []Endpoint {
+	return []Endpoint{
+		Endpoint{"/roll", module.rollDie},
+	}
+}
+
+// Response of the Dice Roll
+type diceResponse struct {
+	Result int `json:"result"`
+}
+
+// The RollDice function
+func (module DiceModule) rollDie(r *http.Request) JSONResponse {
+	value := rand.Intn(5) + 1
+
+	return JSONResponse{
+		http.StatusOK,
+		diceResponse{value},
+	}
+}
